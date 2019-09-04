b1062a487a60091c2599a0a4963693ffd0ca7fc4
feliperflores <feliperflores2@hotmail.com>
2019-09-04 20:18:02 -0300

    Add rmwc components

---
+++ src/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Button } from '@rmwc/button';
+import '@material/button/dist/mdc.button.css';
+
+const Header = () => {
+  return (
+    <div>
+      <Button>Hello World</Button>
+    </div>
+  );
+}
+
+export default Header;
