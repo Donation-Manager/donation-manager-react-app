d089124e06211c45023f89bce2a7423475f9bca5
Matheus Mombach <mombachm@gmail.com>
2019-09-18 00:05:12 -0300

    add grid tiles to home page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '@rmwc/icon/icon.css';
+import { Card, CardPrimaryAction } from '@rmwc/card';
+import { GridList, GridTile, GridTilePrimary, GridTileTitle, GridTileSecondary } from '@rmwc/grid-list';
+import { Typography } from '@rmwc/typography';
+import '@material/grid-list/dist/mdc.grid-list.css';
+import '@material/typography/dist/mdc.typography.css';
 
 const Home: React.FC = () => {
   return (
     <div>
-      <Link to="/donationIntention">
-        <button>Cadastro de Intenções</button>
-      </Link>
+
+    <GridList>
+      <GridTile>
+        <Link to="/donationIntention">
+          <GridTilePrimary>
+          </GridTilePrimary>
+          <GridTileSecondary>
+              <GridTileTitle>
+                Cadastro de Intenções
+              </GridTileTitle>
+          </GridTileSecondary>
+        </Link>
+      </GridTile>
+      <GridTile>
+        <Link to="/donations">
+          <GridTilePrimary>
+          </GridTilePrimary>
+          <GridTileSecondary>
+              <GridTileTitle>
+                Doações
+              </GridTileTitle>
+          </GridTileSecondary>
+        </Link>
+      </GridTile>
+    </GridList>
+
     </div>
   );
 }
